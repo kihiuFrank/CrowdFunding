39cf271d747a941b4dee9311d8d5fbb84f0b8742
kihiuFrank <franklinekihiu@gmail.com>
2023-06-30 13:19:20 +0300

    basic structuring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import { Sidebar, Navbar } from "./components";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+
 function App() {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
       <h1>Test</h1>
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        Navbar
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
