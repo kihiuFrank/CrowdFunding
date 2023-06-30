8bf813466920b896df33c0a769de18e1c74428cf
kihiuFrank <franklinekihiu@gmail.com>
2023-06-30 13:16:10 +0300

    basic structuring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">Sidebar</div>
-
+      <h1>Test</h1>
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         Navbar
       </div>
