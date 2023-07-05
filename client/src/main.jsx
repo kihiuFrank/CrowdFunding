c1488de1c8407a5a988f849dc929a9bea75f394b
kihiuFrank <franklinekihiu@gmail.com>
2023-07-05 20:06:10 +0300

    network fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+// import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 import { StateContextProvider } from "./context";
 import App from "./App";
@@ -10,7 +12,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+  <ThirdwebProvider activeChain={Sepolia}>
     <Router>
       <StateContextProvider>
         <App />
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     networks: {
       hardhat: {},
       sepolia: {
-        url: "https://rpc.ankr.com/eth_sepolia",
+        url: "https://sepolia.rpc.thirdweb.com",
         accounts: [`0x${process.env.PRIVATE_KEY}`],
       },
     },
