d4e37376b1e837945babfead133549555ffcc50d
kihiuFrank <franklinekihiu@gmail.com>
2023-07-07 20:35:08 +0300

    campaign info cards

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -7,11 +7,54 @@ import { useStateContext } from "../context";
 import { CustomButton } from "../components";
 import { calculateBarPercentage, daysLeft } from "../utils";
 import { thirdweb } from "../assets";
+import { CountBox } from "../components";
+
 const CampaignDetails = () => {
   const { state } = useLocation();
-  console.log(state);
+  const { getDonations, contract, address } = useStateContext();
 
-  return <div>CampaignDetails</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState("");
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
+  return (
+    <div>
+      {isLoading && "Loading..."}
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaign"
+            className="w-full h-[410px] object-cover rounded-xl"
+          />
+
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            <div
+              className="absolute h-full bg-[#4acd8d]"
+              style={{
+                width: `${calculateBarPercentage(
+                  state.target,
+                  state.amountCollected
+                )}%`,
+                maxWidth: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox
+            title={`Raised out of ${state.target}`}
+            value={state.amountCollected}
+          />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CampaignDetails;
