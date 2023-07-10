9e19d7de76dbb71ca8ca3afbe5b81be45fd3e781
kihiuFrank <franklinekihiu@gmail.com>
2023-07-10 07:37:26 +0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
-      <p className="font-xl font-bold">Test</p>
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
         <Routes>
