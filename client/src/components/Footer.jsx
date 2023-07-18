4b8393baf68a050810b1669f4b61fa4c41fb616d
kihiuFrank <franklinekihiu@gmail.com>
2023-07-18 14:08:36 +0300

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,11 +5,8 @@ class Footer extends Component {
     return (
       <div className="bg-[#4acd8d] ">
         <div className="flex justify-center items-center text-white bg-[#4acd8d] container mx-auto pt-5 pb-5">
-          Copyright &copy; {1900 + new Date().getYear()}{" "}
-          <a href="https://github.com/kihiuFrank">
-            {" "}
-            kihiuFrank All Rights Reserved.
-          </a>
+          Copyright @{new Date().getFullYear()} by Frankline Kihiu. All rights
+          reserved.
         </div>
       </div>
     );
